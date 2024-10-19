fa1bd6a17128f277e07348f89f1f80df1f98a180
awais-ahmad01 <aahmad19376@gmail.com>
2024-10-19 12:19:20 -0700

    Update vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // This ensures the build files are outputted to the dist folder
+  }
 })
